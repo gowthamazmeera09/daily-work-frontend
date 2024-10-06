bf038e6c8b9123210293d769a4041464d0a28e4d
gowthamazmeera09 <gowthamazmeera@gmail.com>
2024-10-06 11:20:30 +0530

    added one onclikc function in addwork

--- src/pages/Addwork.jsx
+++ src/pages/Addwork.jsx
@@ -75,7 +75,7 @@ function Addwork() {
                 </div>
                 <div className="mb-5">
                     <label htmlFor="work" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Images</label>
-                    <input type="file" onChange={(e) => setSelectedFile(e.target.files[0])} className="shadow-sm bg-gray-50 border text-gray-900 text-sm rounded-lg block w-full p-2.5" required />
+                    <input type="file" onChange={(e) => setSelectedFile(e.target.files[0])} onClick={(e)=>setSelectedFile(e.target.files[0])} className="shadow-sm bg-gray-50 border text-gray-900 text-sm rounded-lg block w-full p-2.5" required />
                 </div>
                 <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5">Add Work</button>
             </form>
